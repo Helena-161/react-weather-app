58e19af2036e77d43be395d2db2dc2a19e2bb846
Helena-161 <stalbovska.helena@gmail.com>
2021-06-27 23:32:34 +0200

    Added units (C and F) in FeelsLikeTemperature component

--- src/FeelsLikeTemperature.js
+++ src/FeelsLikeTemperature.js
@@ -5,8 +5,10 @@ export default function FeelsLikeTemperature(props) {
     let temperature = Math.round(props.tempCelsius);
     if (props.unit !== "celsius") {
       temperature = Math.round((temperature * 9) / 5 + 32);
+      return `Feels like: ${temperature} °F`;
+    } else {
+      return `Feels like: ${temperature} °C`;
     }
-    return `Feels like: ${temperature} °`;
   }
   return <p className="FeelsLikeTemperature">{feelsLikeTemp()}</p>;
 }
