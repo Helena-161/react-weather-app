f139c6ba19b2f8c6ab4d4def176bb35d012dbeda
Helena-161 <stalbovska.helena@gmail.com>
2021-06-27 23:59:41 +0200

    updated styling and rounded wind-speed

--- src/Footer.js
+++ src/Footer.js
@@ -4,23 +4,21 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="Footer text-center my-2">
-      <p>
-        <a
-          href="https://github.com/Helena-161/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by{" "}
-        <a
-          href="https://www.linkedin.com/in/helena-stalbovska-b51435181/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Helena Stalbovska
-        </a>
-      </p>
+      <a
+        href="https://github.com/Helena-161/react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>{" "}
+      by{" "}
+      <a
+        href="https://www.linkedin.com/in/helena-stalbovska-b51435181/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Helena Stalbovska
+      </a>
     </footer>
   );
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -9,22 +9,24 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <LastUpdated date={props.apiData.date} />
-      <h1 className="city-country">
-        {props.apiData.city}, {props.apiData.country}
-      </h1>
-      <CurrentTemperature
-        celsius={props.apiData.temperature}
-        unit={props.unit}
-        setUnit={props.setUnit}
-      />
-      <img src={props.apiData.icon} alt={props.apiData.description} />
-      <h3 className="description">{props.apiData.description}</h3>
+      <div className="text-center">
+        <h1 className="city-country">
+          {props.apiData.city}, {props.apiData.country}
+        </h1>
+        <CurrentTemperature
+          celsius={props.apiData.temperature}
+          unit={props.unit}
+          setUnit={props.setUnit}
+        />
+        <img src={props.apiData.icon} alt={props.apiData.description} />
+        <h3 className="description">{props.apiData.description}</h3>
+      </div>
       <FeelsLikeTemperature
         tempCelsius={props.apiData.feelslike}
         unit={props.unit}
       />
       <p>Humidity: {props.apiData.humidity} %</p>
-      <p>Wind: {props.apiData.wind} m/s</p>
+      <p>Wind: {Math.round(props.apiData.wind)} m/s</p>
     </div>
   );
 }
